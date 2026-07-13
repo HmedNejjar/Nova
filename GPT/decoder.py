a3e33735470ca3024dde84f0b98dc64baa2277e5
BakrM <bakr.m210906@gmail.com>
2026-07-13 18:13:02 +0800

    Add DecoderBlock implementation
    Implement transformer decoder block with multi-head attention, SwiGLU feed-forward network, layer normalization, and residual connections.

---
+++ GPT/decoder.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+from attention import BatchedMultiHeadAttention
+
+class DecoderBlock(nn.Module):
+    def __init__(self, embed_dim: int, num_heads: int, max_seq_len: int, rope_base: int) -> None:
+        super().__init__()
+        
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.max_seq_len = max_seq_len
+        
+        hidden_dim = int(2 * (4 * embed_dim) / 3)
+        
+        # Linear functions for FFN computation
+        self.linear1 = nn.Linear(embed_dim, hidden_dim) # Gate projection
+        self.linear2 = nn.Linear(hidden_dim, embed_dim) # project back to embed_dim
+        self.linear3 = nn.Linear(embed_dim, hidden_dim) # Value projection
+        
+        # LayerNorms for pre-attention and pre-FFN
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        
+        # Batched MHA instance
+        self.MultiHeadAttention = BatchedMultiHeadAttention(embed_dim= embed_dim, num_heads= num_heads, max_seq_len= max_seq_len, rope_base= rope_base)
+        
+    def forward(self, X: Tensor) -> Tensor:
+        # 1. Normalize X
+        X_norm = self.norm1(X)
+        
+        # 2. Apply Multi-Head Attention
+        attn_out = self.MultiHeadAttention(X_norm)
+        
+        # 3. Add residual connection
+        X = X + attn_out
+        
+        # 4. Normalize again
+        X_norm = self.norm2(X)
+        
+        # 5. Apply FFN
+        ffn_out = self.FFN_SwiGLU(X_norm)
+        
+        # 6. Add residual connection
+        X = X + ffn_out
+        
+        return X
+    
+    def FFN_SwiGLU(self, x: Tensor) -> Tensor:
+        """
+        Feed ForwarD Network with SwiGLU activation function.
+
+        Args:
+            x: Input tensor of shape (batch_size, seq_len T, embed_dim d)
+        Returns:
+            Tensor of shape (batch_size, seq_len T, embed_dim d) after applying SwiGLU
+        """
+        gate = self.linear1(x)
+        swish = gate * torch.sigmoid(gate)
+        value = self.linear3(x)
+        
+        swiglu = swish * value
+        
+        return self.linear2(swiglu)
